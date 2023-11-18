d195de6de4d777000585c9cead7ac995b4906cd8
csktan <csktan@mail.com>
2023-11-18 22:50:49 +0100

    changed config_zsh to download auto-suggestionplugin

--- zsh/config_zsh.sh
+++ zsh/config_zsh.sh
@@ -13,25 +13,25 @@
 # "*
 # "* =====================================================================================*/
 
-vim_install_check= sh -c "command zsh --version"
-    if [ -n "$vim_install_check" ]; then 
+zsh_install_check= sh -c "command zsh --version"
+    if [ -n "$zsh_install_check" ]; then 
         echo "Please install zsh first with command: apt install zsh"
         exit
     else
         echo "Downloading Plugins"
     fi 
-plugins=("VundleVim/Vundle.vim" "flazz/vim-colorschemes" "ycm-core/YouCompleteMe" "easymotion/vim-easymotion" "michaeljsmith/vim-indent-object" "maralla/validator.vim" "vim-scripts/c.vim" "vim-airline/vim-airline" "vim-airline/vim-airline-themes" "airblade/vim-gitgutter" "kien/ctrlp.vim" "jceb/vim-orgmode") 
+plugins=("zsh-autosuggestions")
 TOT=${#plugins[@]}
 for(( i=0 ; i<$TOT; i ++)); 
 do  
-    dir="${plugins[i]}"
+    plugin="${plugins[i]}"
     name=$(echo $dir | rev | cut -d '/' -f 1 | rev)
-if [ -d "$HOME/.vim/bundle/$name" ]; then
-        echo "repo" $dir "already exist!!!"
+if [ -d "$HOME/.oh-my-zsh/" ]; then
+        echo "repo" $plugin "already exist!!!"
     else 
         repo=https://github.com/${plugins[i]}.git 
-        git clone $repo  $HOME/.vim/bundle/$name 
+        git clone $repo  $HOME/.oh-my-zsh/plugins/
     fi
 done
-cp vimrc $HOME/.vimrc 
+cp zshrc $HOME/.zshrc 
 
